1dd5e7ad79f11a1f059b500a0554b8038f4512b3
hector918 <hector_zhong@outlook.com>
2023-04-22 15:06:48 -0400

    da

--- router.js
+++ router.js
@@ -265,7 +265,7 @@ function preprocess_req(request, callback) {
 }
 function preprocess_res(request) {
   request.steps = [{ mark: "on request", time: new Date().getTime() }];// lapse of time
-  // for(let key in default_header){ request.res.setHeader(key, default_header[key]);}
+  for(let key in default_header){ request.res.setHeader(key, default_header[key]);}
   
   request.res.error = (error) => { 
     on_error(error);
@@ -277,8 +277,11 @@ function preprocess_res(request) {
     return request;
   }//
   request.send = (data) => {
-    request.res.writeHead(200,default_header);
-    if (!request.isSent) request.res.end(data);
+    // request.res.writeHead(200,default_header);
+    if (!request.isSent) {
+      request.res.write(data);
+      request.res.end();
+    }
     request.isSent = true;
     if(routing.addons.logs) routing.addons.logs.request_log_to_file(request);//log
   };//
