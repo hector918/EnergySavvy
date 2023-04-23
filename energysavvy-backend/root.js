cd016ec133d836f7cd8a8bb0efd9522aa7daaf9e
hector918 <hector_zhong@outlook.com>
2023-04-23 09:25:28 -0400

    testing file server

--- energysavvy-backend/root.js
+++ energysavvy-backend/root.js
@@ -2,5 +2,5 @@
 module.exports = function root(energysavvy) {
   energysavvy.use('/api', require('./controllers/api'));
 
-  energysavvy.serveFile("/", './EnergySavvy-frondend/energysavvy/build');
+  // energysavvy.serveFile("/", './EnergySavvy-frondend/energysavvy/build');
 }
